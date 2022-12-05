f1ad351c93f5090dd324b838cc3f3cc1c847838b
hideayard <hideayard@gmail.com>
2022-12-05 13:22:40 +0700

    clean menu

--- views/layouts/dashboard.php
+++ views/layouts/dashboard.php
@@ -74,59 +74,6 @@ $a = Yii::$app->controller->action->id;
                 </div>
             </form>
 
-            <!-- Right navbar links -->
-            <!-- <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <i class="far fa-comments"></i>
-                        <span class="badge badge-danger navbar-badge">3</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <a href="#" class="dropdown-item">
-                            <div class="media">
-                                <img src="<?= $baseUrl ?>/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-                                <div class="media-body">
-                                    <h3 class="dropdown-item-title">
-                                        Admin
-                                        <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                                    </h3>
-                                    <p class="text-sm">Welcome to Digital TVET Academy</p>
-                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                </div>
-                            </div>
-                        </a>
-
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <i class="far fa-bell"></i>
-                        <span class="badge badge-warning navbar-badge">2</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <span class="dropdown-item dropdown-header">1 Notifications</span>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-envelope mr-2"></i> 1 new messages
-                            <span class="float-right text-muted text-sm">3 mins</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-users mr-2"></i> 0 friend requests
-                            <span class="float-right text-muted text-sm">12 hours</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-file mr-2"></i> 1 new reports
-                            <span class="float-right text-muted text-sm">2 days</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                    </div>
-                </li>
-            </ul> -->
         </nav>
         <!-- /.navbar -->
 
@@ -155,8 +102,6 @@ $a = Yii::$app->controller->action->id;
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                        <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
 
                         <li class="nav-header">MAIN MENU</li>
                         <li class="nav-item">
@@ -166,13 +111,15 @@ $a = Yii::$app->controller->action->id;
                             </a>
                         </li>
 
-                        <?php if (Yii::$app->user->identity->user_tipe == "ADMIN" || Yii::$app->user->identity->user_tipe == 'TRAINER') : ?>
-                            <!-- <li class="nav-item">
-                                <a href="<?= Url::to(['courses/index']) ?>" class="nav-link <?= ($c == "courses") ? "active" : "" ?>">
-                                    <i class="nav-icon far fa-address-card"></i>
-                                    <p>Courses</p>
+                        <li class="nav-item">
+                                <a href="<?= Url::to(['log/index']) ?>" class="nav-link <?= ($c == "log") ? "active" : "" ?>">
+                                    <i class="nav-icon fa fa-chart-line"></i>
+                                    <p>Data Logs</p>
                                 </a>
-                            </li> -->
+                            </li> 
+
+                        <?php if (Yii::$app->user->identity->user_tipe == "ADMIN" || Yii::$app->user->identity->user_tipe == 'TRAINER') : ?>
+
                             <li class="nav-item">
                                 <a href="<?= Url::to(['users/index']) ?>" class="nav-link <?= ($c == "users") ? "active" : "" ?>">
                                     <i class="nav-icon far fa-user"></i>
@@ -181,7 +128,7 @@ $a = Yii::$app->controller->action->id;
                             </li>
                         <?php endif; ?>
 
-                        <?php if (Yii::$app->user->identity->user_tipe == "ADMIN") : ?>
+                        <?php if (Yii::$app->user->identity->user_tipe == "ADMIN" || Yii::$app->user->identity->user_tipe == "SUPERADMIN") : ?>
 
                             <li class="nav-item">
                                 <a href="<?= Url::to(['banner/index']) ?>" class="nav-link <?= ($c == "banner") ? "active" : "" ?>">
@@ -189,20 +136,6 @@ $a = Yii::$app->controller->action->id;
                                     <p>Banners</p>
                                 </a>
                             </li>
-
-                            <!-- <li class="nav-item">
-                                <a href="<?= Url::to(['coursesession/index']) ?>" class="nav-link <?= ($c == "coursesession") ? "active" : "" ?>">
-                                    <i class="nav-icon far fa-hourglass"></i>
-                                    <p>Courses Sessions</p>
-                                </a>
-                            </li -->
-
-                            <li class="nav-item">
-                                <a href="<?= Url::to(['log/index']) ?>" class="nav-link <?= ($c == "log") ? "active" : "" ?>">
-                                    <i class="nav-icon fa fa-chart-line"></i>
-                                    <p>Data Logs</p>
-                                </a>
-                            </li> 
 
                             <li class="nav-item">
                                 <a href="<?= Url::to(['node/index']) ?>" class="nav-link <?= ($c == "node") ? "active" : "" ?>">
